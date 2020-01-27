d5a1a452ec20b963c35f2b9e08539f9457692993
Brian <briankeefe@umass.edu>
2020-01-27 18:46:35 +0000

    more layout

---
+++ src/GetQuote.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+
+function Quote() {
+	return (
+		<Box>
+			<TextField />
+			<TextField />
+			<TextField />
+			<TextField />
+		</Box>
+	);
+}
