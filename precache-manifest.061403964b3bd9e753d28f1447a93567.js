0fbab303a44c50c56b12862e0b955a2fc2048232
Brian <briankeefe@umass.edu>
2020-01-27 19:37:58 +0000

    Updates

---
+++ precache-manifest.061403964b3bd9e753d28f1447a93567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441391712b2b4098ec60a12fa2aa8d2e",
+    "url": "/shirts/index.html"
+  },
+  {
+    "revision": "b32ea085aece4a3e33cb",
+    "url": "/shirts/static/css/main.818a74d2.chunk.css"
+  },
+  {
+    "revision": "877a43b42290c54bf615",
+    "url": "/shirts/static/js/2.ae9ec213.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/shirts/static/js/2.ae9ec213.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b32ea085aece4a3e33cb",
+    "url": "/shirts/static/js/main.5cc0c034.chunk.js"
+  },
+  {
+    "revision": "17a094cc07a9ae971e1c",
+    "url": "/shirts/static/js/runtime-main.5122283a.js"
+  }
+]);
